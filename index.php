38f78d39267031596a3eeb81fd07600f0a447123
Jon Janelle <jjanelle@g.harvard.edu>
2017-02-07 07:38:50 -0500

    Separated random choice logic from index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
+<?php require('quote_chooser.php'); ?>
 <!doctype html>
-
 <html>
 
   <head>
@@ -11,24 +11,6 @@
   </head>
 
   <body>
-    <?php
-      $quotes = array("Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that. ",
-                      "If things go wrong, don’t go with them.",
-                      "Optimism is the faith that leads to achievement.",
-                      "When the whole world is silent, even one voice becomes powerful.",
-                      "It took me quite a long time to develop a voice, and now that I have it, I am not going to be silent.");
-
-      $authors = array("Martin Luther King, Jr.",
-                       "Roger Babson",
-                       "Helen Keller",
-                       "Malala Yousafzai",
-                       "Madeleine Albright");
-
-      $position = rand(0, count($quotes)-1);
-      $quote = $quotes[$position];
-      $author = $authors[$position];
-    ?>
-
     <div id="container">
       <div id="title" class="jumbotron shadowbox">
         <div class="page-header">
